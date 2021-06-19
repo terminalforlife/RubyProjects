713112e53a8ef4fa8d1c7d133711d9e0f3b9e573
terminalforlife <terminalforlife@yahoo.com>
2021-06-20 00:14:33 +0100

    Improve jan.rb

--- source/jan.rb
+++ source/jan.rb
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - RubyProjects/source/jan.rb
 # Started On        - Sat 19 Jun 20:52:04 BST 2021
-# Last Change       - Sat 19 Jun 21:25:23 BST 2021
+# Last Change       - Sun 20 Jun 00:14:30 BST 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -13,7 +13,6 @@
 # Features:
 #
 #TODO: Check the user's input case-insensitively.
-#TODO: Use array of months and iterate over each to check the first 3 letters.
 #
 # Bugs:
 #
@@ -25,13 +24,18 @@ CurVer = '2021-06-19'
 Progrm = 'jan.rb'
 
 def usage
-	puts('Usage: ' + Progrm + '[OPTS]')
+	puts("Usage: #{Progrm} [OPTS] MONTH_1 [MONTH_2 ...]")
 end
 
 def error(status, string)
 	STDERR.printf("ERROR: %s\n", string)
 	exit(status) if status > 0
 end
+
+months = [
+	'January', 'February', 'March', 'April', 'May', 'June', 'July',
+	'August', 'September', 'October', 'November', 'December'
+]
 
 while ARGV[0]
 	if ARGV[0] == '--help' or ARGV[0] == '-h'
@@ -45,28 +49,10 @@ while ARGV[0]
 	ARGV.shift()
 end
 
-if ARGV[0] == 'Jan'
-	puts('January')
-elsif ARGV[0] == 'Feb'
-	puts('February')
-elsif ARGV[0] == 'Mar'
-	puts('March')
-elsif ARGV[0] == 'Apr'
-	puts('April')
-elsif ARGV[0] == 'May'
-	puts('May')
-elsif ARGV[0] == 'Jun'
-	puts('June')
-elsif ARGV[0] == 'Jul'
-	puts('July')
-elsif ARGV[0] == 'Aug'
-	puts('August')
-elsif ARGV[0] == 'Sep'
-	puts('September')
-elsif ARGV[0] == 'Oct'
-	puts('October')
-elsif ARGV[0] == 'Nov'
-	puts('November')
-elsif ARGV[0] == 'Dec'
-	puts('December')
+ARGV.each() do |arg|
+	for word in months
+		if word[0..2] == arg[0..2]
+			puts(word)
+		end
+	end
 end
